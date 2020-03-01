f064406eeb60d06b176e2b9e4ef28a01f1f6d93e
kzcheng <kevinzifancheng@gmail.com>
2020-02-29 22:30:55 -0800

    Completed Select Panel and stuff

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,11 +29,20 @@ const data = [
 
 
 class Dashboard extends Component {
+
+  // initial state
   state = {
     loading: false,
     focused: null,
   };
 
+  selectPanel = id => {
+    this.setState({
+      focused: id
+    });
+  };
+
+  // render
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
@@ -47,9 +56,9 @@ class Dashboard extends Component {
       .map(panel => (
         <Panel
           key={panel.id}
-          id={panel.id}
           label={panel.label}
           value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
