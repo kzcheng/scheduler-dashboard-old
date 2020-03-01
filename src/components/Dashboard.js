37d43ee9e586ce004eadedb86dadbe457190e543
kzcheng <kevinzifancheng@gmail.com>
2020-02-29 21:36:36 -0800

    Added support for dashboard selection

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,24 +30,28 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null,
   };
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    const panels = data.filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
